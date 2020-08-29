0ced3079d11c89ef103e442d2da98f47cdc60ef9
Luc Engelen <luc@kabisa.nl>
2020-08-29 21:09:14 +0200

    Clean up health-check verticle

--- src/main/java/nl/friendlymirror/top10/healthcheck/HealthCheckVerticle.java
+++ src/main/java/nl/friendlymirror/top10/healthcheck/HealthCheckVerticle.java
@@ -3,26 +3,18 @@ package nl.friendlymirror.top10.healthcheck;
 import java.io.IOException;
 import java.util.Properties;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import com.google.common.annotations.VisibleForTesting;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.http.HttpServer;
-import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.JsonObject;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 public class HealthCheckVerticle extends AbstractVerticle {
-
-    private static final Logger LOGGER = LogManager.getLogger();
 
     private final int port;
 
-    private String commitHash;
     private String responseBody;
-    private HttpServer server;
-    private String version;
 
     @VisibleForTesting HealthCheckVerticle(int port) {
         this.port = port;
@@ -42,7 +34,7 @@ public class HealthCheckVerticle extends AbstractVerticle {
     }
 
     private String getVersion() {
-        Properties properties = new Properties();
+        var properties = new Properties();
         try {
             properties.load(this.getClass().getClassLoader().getResourceAsStream("build.properties"));
             return properties.getProperty("project.version");
@@ -53,12 +45,12 @@ public class HealthCheckVerticle extends AbstractVerticle {
 
     @Override
     public void start() {
-        LOGGER.info("Starting health-check verticle");
+        log.info("Starting health-check verticle");
 
-        server = vertx.createHttpServer();
+        var server = vertx.createHttpServer();
 
-        commitHash = getCommitHash();
-        version = getVersion();
+        var commitHash = getCommitHash();
+        var version = getVersion();
 
         responseBody = new JsonObject()
                 .put("commitHash", commitHash)
@@ -66,16 +58,16 @@ public class HealthCheckVerticle extends AbstractVerticle {
                 .toString();
 
         server.requestHandler(request -> {
-            HttpServerResponse response = request.response();
+            var response = request.response();
             response.putHeader("content-type", "application/json");
             response.end(responseBody);
         });
 
         server.listen(port, ar -> {
             if (ar.succeeded()) {
-                LOGGER.info("listening on port {}", port);
+                log.info("listening on port {}", port);
             } else {
-                LOGGER.error("Unable to listen on port {}", port, ar.cause());
+                log.error("Unable to listen on port {}", port, ar.cause());
             }
         });
     }
