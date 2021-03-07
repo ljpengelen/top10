623f8e99dc9e2f86b90cb88221ca93f6413fe7cc
Luc Engelen <luc@kabisa.nl>
2021-03-07 11:34:19 +0100

    Don't show assignment list for single participant

--- front-end/src/cljs/top10/views/quiz.cljs
+++ front-end/src/cljs/top10/views/quiz.cljs
@@ -6,68 +6,89 @@
             [top10.subs :as subs]
             [top10.views.base :refer [back-to-overview-button]]))
 
+(defn quiz-has-ended []
+  [:<>
+   [:p (str "This quiz has ended")]
+   [back-to-overview-button]])
+
+(defn no-list-submitted []
+  [:<>
+   [:p (str
+        "This quiz has reached the final round, "
+        "but you haven't submitted a top 10. "
+        "That means you can't participate in the final round. "
+        "Better luck next time!")]
+   [back-to-overview-button]])
+
+(defn assign-lists [quiz-id lists] 
+  [:<>
+   [:p
+    "This quiz has reached the final round. "
+    "It's time to assign top 10's to participants."]
+   [grid {:container true :direction "column" :spacing 2}
+    [grid {:item true}
+     [table-container
+      [table
+       [table-head
+        [table-row
+         [table-cell "Assigned to"]
+         [table-cell "Action"]]]
+       [table-body
+        (for [{list-id :id assigneeName :assigneeName} lists]
+          ^{:key list-id}
+          [table-row
+           [table-cell (or assigneeName "Not assigned yet")]
+           [table-cell [link {:href (str "/quiz/" quiz-id "/list/" list-id "/assign") :color "primary"} "Assign"]]])]]]]
+    [grid {:item true}
+     [back-to-overview-button]]]])
+
+(defn first-round-in-progress [number-of-participants deadline quiz-id personal-list-has-draft-status? personal-list-id]
+  [:<>
+   [:p
+    (str
+     "At the moment, this quiz has " number-of-participants " " (if (= number-of-participants 1) "participant" "participants") ". "
+     "Anyone who wants to join has until " deadline " to submit their personal top 10. "
+     "If you know anyone who might also want to join, just share the following URL: ")]
+   [:pre (str front-end-base-url "/quiz/" quiz-id "/join")]
+   (case personal-list-has-draft-status?
+     (true) [:p (str
+                 "Remember, you still have to submit your personal top 10 for this quiz! "
+                 "You can only take part in the final round after you've submitted a top 10.")]
+     (false) [:p (str "You've already submitted your personal top 10 for this quiz.")]
+     (nil) [button {:color "primary"
+                    :on-click #(rf/dispatch [::events/participate-in-quiz quiz-id])
+                    :variant "contained"}
+            "Participate in quiz"])
+   (when (some? personal-list-has-draft-status?)
+     [grid {:container true :spacing 2}
+      [grid {:item true}
+       [button {:href (str "/quiz/" quiz-id "/list/" personal-list-id "/personal") :color "primary" :variant "contained"}
+        (if personal-list-has-draft-status? "Submit top 10" "View top 10")]]
+      [grid {:item true}
+       [back-to-overview-button]]])])
+
+(defn single-participant []
+  [:<>
+   [:p (str
+        "This quiz has reached the final round, "
+        "but you're the only participant. "
+        "Better luck next time!")]
+   [back-to-overview-button]])
+
 (defn quiz-page [loading-quiz? {:keys [name deadline deadline-has-passed? id isActive personalListId personalListHasDraftStatus]} number-of-participants lists]
   (when-not loading-quiz?
     [:<>
      [:h1 name]
      (cond
-       (not isActive)
-       [:<>
-        [:p (str "This quiz has ended")]
-        [back-to-overview-button]]
-       (and deadline-has-passed? (or (nil? personalListHasDraftStatus) personalListHasDraftStatus))
-       [:<>
-        [:p (str
-             "This quiz has reached the final round, "
-             "but you haven't submitted a top 10. "
-             "That means you can't participate in the final round. "
-             "Better luck next time!")]
-        [back-to-overview-button]]
-       (and deadline-has-passed? personalListId (false? personalListHasDraftStatus))
-       [:<>
-        [:p (str
-             "This quiz has reached the final round. "
-             "It's time to assign top 10's to participants.")]
-        [grid {:container true :direction "column" :spacing 2}
-         [grid {:item true}
-          [table-container
-           [table
-            [table-head
-             [table-row
-              [table-cell "Assigned to"]
-              [table-cell "Action"]]]
-            [table-body
-             (for [{list-id :id assigneeName :assigneeName} lists]
-               ^{:key id}
-               [table-row
-                [table-cell (or assigneeName "Not assigned yet")]
-                [table-cell [link {:href (str "/quiz/" id "/list/" list-id "/assign") :color "primary"} "Assign"]]])]]]]
-         [grid {:item true}
-          [back-to-overview-button]]]]
-       (not deadline-has-passed?)
-       [:<>
-        [:p
-         (str
-          "At the moment, this quiz has " number-of-participants " " (if (= number-of-participants 1) "participant" "participants") ". "
-          "Anyone who wants to join has until " deadline " to submit their personal top 10. "
-          "If you know anyone who might also want to join, just share the following URL: ")]
-        [:pre (str front-end-base-url "/quiz/" id "/join")]
-        (case personalListHasDraftStatus
-          (true) [:p (str
-                      "Remember, you still have to submit your personal top 10 for this quiz! "
-                      "You can only take part in the final round after you've submitted a top 10.")]
-          (false) [:p (str "You've already submitted your personal top 10 for this quiz.")]
-          (nil) [button {:color "primary"
-                         :on-click #(rf/dispatch [::events/participate-in-quiz id])
-                         :variant "contained"}
-                 "Participate in quiz"])
-        (when (some? personalListHasDraftStatus)
-          [grid {:container true :spacing 2}
-           [grid {:item true}
-            [button {:href (str "/quiz/" id "/list/" personalListId "/personal") :color "primary" :variant "contained"}
-             (if personalListHasDraftStatus "Submit top 10" "View top 10")]]
-           [grid {:item true}
-            [back-to-overview-button]]])])]))
+       (not isActive) [quiz-has-ended]
+       (not deadline-has-passed?) [first-round-in-progress number-of-participants deadline id personalListHasDraftStatus personalListId]
+       (or (nil? personalListHasDraftStatus) personalListHasDraftStatus) [no-list-submitted]
+       (and personalListId (false? personalListHasDraftStatus) (> 0 number-of-participants)) [assign-lists id lists]
+       :else [single-participant])]))
 
 (defn quiz-page-container []
-  [quiz-page @(rf/subscribe [::subs/loading-quiz?]) @(rf/subscribe [::subs/quiz]) @(rf/subscribe [::subs/number-of-participants]) @(rf/subscribe [::subs/quiz-lists])])
+  [quiz-page
+   @(rf/subscribe [::subs/loading-quiz?])
+   @(rf/subscribe [::subs/quiz])
+   @(rf/subscribe [::subs/number-of-participants])
+   @(rf/subscribe [::subs/quiz-lists])])
