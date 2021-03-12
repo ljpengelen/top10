3889b2285821864da18eb24eb250d60cc115ee13
Luc Engelen <luc@kabisa.nl>
2021-03-12 23:09:46 +0100

    Only show videos once data is loaded

--- front-end/src/cljs/top10/views/list.cljs
+++ front-end/src/cljs/top10/views/list.cljs
@@ -4,19 +4,22 @@
             [top10.subs :as subs]
             [top10.views.base :refer [embedded-video]]))
 
-(defn list-page [quiz-id creator-name has-draft-status? videos]
-  [:<>
-   [:h1 creator-name]
-   (if has-draft-status?
-     [:p (str creator-name " did not submit a top 10 for this quiz.")]
-     [:<>
-      [:p (str creator-name " submitted these 10 songs for this quiz.")]
-      [:div {:class "ytEmbeddedContainer"}
-       [embedded-video (first videos) videos]]])
-   [button {:href (str "/quiz/" quiz-id "/results")} "Back to quiz results"]])
+(defn list-page [loading-quiz? loading-list? quiz-id creator-name has-draft-status? videos]
+  (when-not (or loading-quiz? loading-list?)
+    [:<>
+     [:h1 creator-name]
+     (if has-draft-status?
+       [:p (str creator-name " did not submit a top 10 for this quiz.")]
+       [:<>
+        [:p (str creator-name " submitted these 10 songs for this quiz.")]
+        [:div {:class "ytEmbeddedContainer"}
+         [embedded-video (first videos) videos]]])
+     [button {:href (str "/quiz/" quiz-id "/results")} "Back to quiz results"]]))
 
 (defn list-page-container []
   [list-page
+   @(rf/subscribe [::subs/loading-quiz?])
+   @(rf/subscribe [::subs/loading-list?])
    @(rf/subscribe [::subs/active-quiz])
    @(rf/subscribe [::subs/list-creator-name])
    @(rf/subscribe [::subs/has-draft-status])
