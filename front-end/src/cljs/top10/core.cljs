c8f61a3d44116e28a1d15d59875be3b4f0a0c5dc
Luc Engelen <luc@kabisa.nl>
2021-03-21 12:07:36 +0100

    Send error logging to Sentry

--- front-end/src/cljs/top10/core.cljs
+++ front-end/src/cljs/top10/core.cljs
@@ -1,5 +1,6 @@
 (ns top10.core
   (:require ["@sentry/browser" :as Sentry]
+            ["@sentry/integrations" :as SentryIntegrations]
             [day8.re-frame.async-flow-fx]
             [day8.re-frame.http-fx]
             [re-frame.core :as rf]
@@ -15,7 +16,8 @@
     (println "dev mode")))
 
 (defn set-up-sentry []
-  (.init Sentry #js {:dsn "https://af15232561f8401b941d26f709e51f17@o136594.ingest.sentry.io/5403220"}))
+  (.init Sentry #js {:dsn "https://af15232561f8401b941d26f709e51f17@o136594.ingest.sentry.io/5403220"
+                     :integrations #js [(new SentryIntegrations/CaptureConsole #js {:levels #js ["error"]})]}))
 
 (defn ^:dev/after-load mount-root []
   (rf/clear-subscription-cache!)
