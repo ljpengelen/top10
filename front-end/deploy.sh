1f15d85877c8bef5ec6836c1081ddf55337b9f61
Luc Engelen <16940687+ljpengelen@users.noreply.github.com>
2024-10-01 22:52:08 +0200

    Export environment variables

--- front-end/deploy.sh
+++ front-end/deploy.sh
@@ -2,16 +2,16 @@
 
 set -euo pipefail
 
-GIT_COMMIT_HASH="$(git rev-parse --short HEAD)"
-GIT_COMMIT_COUNT="$(git rev-list HEAD --count)"
-VERSION="v0.1.$GIT_COMMIT_COUNT.$GIT_COMMIT_HASH"
+export GIT_COMMIT_HASH="$(git rev-parse --short HEAD)"
+export GIT_COMMIT_COUNT="$(git rev-list HEAD --count)"
+export VERSION="v0.1.$GIT_COMMIT_COUNT.$GIT_COMMIT_HASH"
 
-API_BASE_URL=https://top10-api.cofx.nl
-FRONT_END_BASE_URL=https://top10.cofx.nl
-GOOGLE_OAUTH2_CLIENT_ID=442497309318-72n7detrn1ne7bprs59fv8lsm6hsfivh.apps.googleusercontent.com
-GOOGLE_OAUTH2_REDIRECT_URI=https://top10.cofx.nl/oauth2/google
-MICROSOFT_OAUTH2_CLIENT_ID=1861cf5d-8a7f-4c90-88ec-b4bdbb408b61
-MICROSOFT_OAUTH2_REDIRECT_URI=https://top10.cofx.nl/oauth2/microsoft
+export API_BASE_URL=https://top10-api.cofx.nl
+export FRONT_END_BASE_URL=https://top10.cofx.nl
+export GOOGLE_OAUTH2_CLIENT_ID=442497309318-72n7detrn1ne7bprs59fv8lsm6hsfivh.apps.googleusercontent.com
+export GOOGLE_OAUTH2_REDIRECT_URI=https://top10.cofx.nl/oauth2/google
+export MICROSOFT_OAUTH2_CLIENT_ID=1861cf5d-8a7f-4c90-88ec-b4bdbb408b61
+export MICROSOFT_OAUTH2_REDIRECT_URI=https://top10.cofx.nl/oauth2/microsoft
 
 rm -rf dist
 git clone https://github.com/ljpengelen/top10.git -b deploy dist
