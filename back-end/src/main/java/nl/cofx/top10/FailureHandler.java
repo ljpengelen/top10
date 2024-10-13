f1d8576e45fe2a9471e2f560fe0d02d3cd9f4b50
Luc Engelen <16940687+ljpengelen@users.noreply.github.com>
2024-10-13 21:23:03 +0200

    Log request body, URI, and headers in case of failure

--- back-end/src/main/java/nl/cofx/top10/FailureHandler.java
+++ back-end/src/main/java/nl/cofx/top10/FailureHandler.java
@@ -11,14 +11,34 @@ public class FailureHandler {
 
     private static void handleFailure(RoutingContext routingContext) {
         var route = routingContext.currentRoute();
-        var statusCode = routingContext.statusCode();
+        var path = route.getPath();
+        var methods = route.methods();
+
         var failure = routingContext.failure();
-        log.error("Unexpected failure when handling route for path {} and methods {} resulted in status code {}",
-                route.getPath(), route.methods(), statusCode, failure);
+
         if (failure != null) {
+            log.error("Unexpected failure when handling route for path {} and methods {}",
+                    path, methods, failure);
             Respond.withErrorMessage(routingContext, 500, "Internal server error");
             return;
         }
+
+        var request = routingContext.request();
+        request.body(asyncBuffer -> {
+            if (asyncBuffer.failed()) {
+                log.error("Unable to retrieve body of failed request", asyncBuffer.cause());
+                return;
+            }
+
+            var headers = request.headers();
+            var uri = request.absoluteURI();
+            var body = asyncBuffer.result().toString();
+            log.error("Failed request has uri {}, headers {}, and body {}", uri, headers, body);
+        });
+
+        var statusCode = routingContext.statusCode();
+        log.error("Unexpected failure when handling route for path {} and methods {} resulted in status {}",
+                path, methods, statusCode);
 
         Respond.withErrorMessage(routingContext, statusCode);
     }
