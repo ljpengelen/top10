60744fb6ab64626f006906494c818675a514fcd5
Luc Engelen <16940687+ljpengelen@users.noreply.github.com>
2026-06-27 23:25:33 +0200

    Replace unclear playlists with explicit lists of videos

--- front-end/src/cljs/top10/views/list.cljs
+++ front-end/src/cljs/top10/views/list.cljs
@@ -2,7 +2,7 @@
   (:require [re-frame.core :as rf]
             [reagent-mui.components :refer [button]]
             [top10.subs :as subs]
-            [top10.views.base :refer [embedded-video]]))
+            [top10.views.base :refer [embedded-videos]]))
 
 (defn list-page [loading-quiz? loading-list? quiz-id creator-name has-draft-status? videos]
   (when-not (or loading-quiz? loading-list?)
@@ -12,8 +12,7 @@
        [:p (str creator-name " did not submit a top 10 for this quiz.")]
        [:<>
         [:p (str creator-name " submitted these 10 songs for this quiz.")]
-        [:div {:class "ytEmbeddedContainer"}
-         [embedded-video (first videos) videos]]])
+        [embedded-videos videos]])
      [button {:href (str "/quiz/" quiz-id "/results")} "Back to quiz results"]]))
 
 (defn list-page-container []
